30d9f68de218a6001a21ac6dc7808efdfd31e544
maykiscofield <ozgurkuzun0@gmail.com>
2026-01-03 00:11:06 +0300

    Analytics eklendi

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react';
 import Pomodoro from "./pages/Pomodoro";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
@@ -33,10 +34,11 @@ const App = () => (
               <Route path="/iletisim" element={<Iletisim />} />
               <Route path="/sepet" element={<Cart />} />
               <Route path="/panel" element={<Dashboard />} />
-              {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
               <Route path="*" element={<NotFound />} />
             </Routes>
           </BrowserRouter>
+          {/* Vercel Analytics buraya eklendi */}
+          <Analytics /> 
         </CartProvider>
       </AuthProvider>
     </TooltipProvider>
